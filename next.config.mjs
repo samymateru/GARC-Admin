40fb4c264723c2d13573d5be084f4e9574c83702
Samuel Alfred <samymateru1999@gmail.com>
2025-01-20 14:03:04 +0300

    ignore eslint

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: false,
     images: {
         domains: ['img.freepik.com'], // Add external image domains here
-      },
+  },
+    eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
